78569a60d84b9ee5491a6edce3c1db19e3ff8592
Mehrojiddin <mehrojiarkom@gmail.com>
2022-07-26 23:59:39 +0500

    Task 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Home task 27.07\
+
+## 1 Doublefuctorial
---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function fuctorial(a) {
+    if (a==1){  
+return 1
+}
+
+else 
+return a * fuctorial(a-2)
+}
+
+console.log(fuctorial(9));
